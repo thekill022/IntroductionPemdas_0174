7309f8d7843e85fc6d33cf67dbd1d5ae95e5dc93
thekill022 <gibfathoni@gmail.com>
2026-02-23 11:14:46 +0700

    membuat program main

--- app.cpp
+++ app.cpp
@@ -2,3 +2,11 @@
 // using namespace std;
 using std::cout;
 using std::endl;
+
+int main() // program utama
+{
+    // menampilkan string
+    cout << "selamat datang di TI UMY" << endl;
+    // menampilkan numeris
+    cout << 10;
+}
